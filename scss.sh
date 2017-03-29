85003002e143c125e2c1b4682002e5c5e578755a
David Espinoza <despinoza1027@gmail.com>
2017-03-29 11:17:23 -0500

    fixed scss errors across site

--- scss.sh
+++ scss.sh
@@ -1,4 +1,4 @@
 # this will automatically set "--watch"
 
-node-sass style/style.scss style/style.css
-node-sass style/style.scss style/style.css --watch
+node-sass style/style.scss style.css
+node-sass style/style.scss style.css --watch
